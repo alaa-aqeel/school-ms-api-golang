0a0a95418f6c6d45257716b66bc988cfcc682b88
Alaa Aqeel <alaa.21.iraq@gmail.com>
2024-03-01 04:32:35 +0300

    new file:   api/middlewares/auth_middleware.go 	modified:   api/v1/handler/auth/login_handler.go 	modified:   api/v1/route.go 	modified:   go.mod 	modified:   go.sum 	new file:   internal/auth/jwt.go

--- api/v1/route.go
+++ api/v1/route.go
@@ -1,11 +1,14 @@
 package v1
 
 import (
+	"github.com/alaa-aqeel/school-ms-api-golang/api/middlewares"
+	"github.com/alaa-aqeel/school-ms-api-golang/api/v1/handler/auth"
 	"github.com/go-chi/chi/v5"
 )
 
 func Route() *chi.Mux {
 	r := chi.NewRouter()
-
+	r.Use(middlewares.AuthMiddleware)
+	r.Post("/login", auth.LoginHandler)
 	return r
 }
